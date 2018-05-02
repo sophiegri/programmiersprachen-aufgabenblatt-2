c0d56d26ea15cff5d7e83ed0b17b19df7d2ec527
sophiegri <sophiegrimme@gmx.de>
2018-05-02 21:09:36 +0200

    Add tests for Vec2

--- source/tests.cpp
+++ source/tests.cpp
@@ -4,17 +4,88 @@
 
 //Aufgabe 2.2
 
-
 TEST_CASE ("Vector" ,"[vec]")
 {
   Vec2 vector1 {}; //Hier wird ein Objekt mit dem Standardkonstruktor angelegt 
   Vec2 vector2 {2.4,1.5}; //Hier wird ein Objekt mit dem Konstruktor erstellt
-  REQUIRE (Vec2 (2.4, 1.5).x == Approx(2.4));
-  REQUIRE (vector2.x == Approx (2.4));
-  REQUIRE (Vec2 (-9.7, 9.0).y == Approx(9.0));
-  REQUIRE (vector1.y == Approx (0.0));
-
+  REQUIRE (Vec2 (2.4, 1.5).x == Approx(2.4).epsilon(0.01));
+  REQUIRE (vector2.x == Approx (2.4).epsilon(0.01));
+  REQUIRE (Vec2 (-9.7, 9.0).y == Approx(9.0).epsilon(0.01));
+  REQUIRE (vector1.y == Approx (0.0).epsilon(0.01));
 }
+
+//Aufgabe 2.3
+
+TEST_CASE ("Vektoraddition" ,"[vec]" "[addition]")
+{
+ Vec2 vector1 {4.5, 8.9};
+ Vec2 vector2 {2.8, 0.5}; 
+ vector1+=vector2;
+ REQUIRE (vector1.x== Approx(7.3f).epsilon(0.01));
+ REQUIRE (vector1.y== Approx(9.4f).epsilon(0.01));
+
+ Vec2 vector3 {0.0, -4.7};
+ Vec2 vector4 {0.2, -5}; 
+ vector3+=vector4;
+ REQUIRE (vector3.x== Approx(0.2f).epsilon(0.01));
+ REQUIRE (vector3.y== Approx(-9.7f).epsilon(0.01));
+}
+
+TEST_CASE ("Vektorsubtraktion" ,"[vec]" "[subtraktion]")
+{
+ Vec2 vector1 {4.5, 8.9};
+ Vec2 vector2 {2.8, 0.5}; 
+ vector1-=vector2;
+
+ REQUIRE (vector1.x== Approx (1.7f).epsilon(0.01));
+ REQUIRE (vector1.y== Approx (8.4f).epsilon(0.01));
+
+ Vec2 vector3 {0.0, -4.7};
+ Vec2 vector4 {0.2, -5}; 
+ vector3-=vector4;
+ REQUIRE (vector3.x== Approx (-0.2f).epsilon(0.01));
+ REQUIRE (vector3.y== Approx (0.3f).epsilon(0.01));
+}
+
+TEST_CASE ("Vektormultiplikation" ,"[vec]" "[multiplikation]")
+{
+ Vec2 vector1 {4.5, 8.9};
+ float s = 9.0f;
+ vector1 *= s;
+
+ REQUIRE (vector1.x== Approx (40.5f).epsilon(0.01));
+ REQUIRE (vector1.y== Approx (80.1f).epsilon(0.01));
+
+ Vec2 vector2 {0.0, -4.7}; 
+ float p = -4.8f;
+ vector2 *= p;
+ REQUIRE (vector2.x== Approx (0.0f).epsilon(0.01));
+ REQUIRE (vector2.y== Approx (22.56f).epsilon(0.01));
+}
+
+TEST_CASE ("Vektordivision" ,"[vec]" "[division]")
+{
+ Vec2 vector1 {4.5, 8.9};
+ float s = 9.0f;
+ vector1 /= s;
+
+ REQUIRE (vector1.x== Approx (0.5f).epsilon(0.01));
+ REQUIRE (vector1.y== Approx (0.98f).epsilon(0.01));
+
+ Vec2 vector2 {0.0, -4.7}; 
+ float p = -4.8f;
+ vector2 /= p;
+ REQUIRE (vector2.x== Approx (0.0f).epsilon(0.01));
+ REQUIRE (vector2.y== Approx (0.97f).epsilon(0.01));
+
+ Vec2 vector3 {0.4, 3}; 
+ float q = 0.0f;
+ vector2 /= p;
+ REQUIRE (vector3.x== Approx (vector3.x).epsilon(0.01));
+ REQUIRE (vector3.y== Approx (vector3.y).epsilon(0.01));
+}
+
+
 
 int main(int argc, char *argv[])
 {
